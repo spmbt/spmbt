392cc584bb0fd3eef2efe4c81d28887412c7f65f
spmbt <spmbt0@gmail.com>
2013-11-11 08:41:39 +0400

    Merge  into gh-pages

---
+++ wk/core.js
@@ -0,0 +1,90 @@
+﻿var $q = function(q, el){return (el || document).querySelector(q)}  //=====общие функции=====
+,$qA = function(q, el){return (el || document).querySelectorAll(q)}
+,$x = function(el, h){	if(h) for(var i in h) el[i] = h[i]; return el}
+,$pd = function(ev){ev.preventDefault();}
+,$sp = function(ev){ev.stopPropagation();}
+,$e = function(h){ //el|clone,IF+ifA,q|[q,el],cl,ht,cs,at,on,apT,prT,bef,aft,f+fA
+	if(h.ht || h.at){
+		var at = h.at ||{}; if(h.ht) at.innerHTML = h.ht;}
+	if(typeof h.IF =='function')
+		h.IF = h.IF.apply(h, h.ifA ||[]);
+	h.el = h.el || h.clone || h.IF && h.IF.attributes && h.IF ||'DIV';
+	var o = h.el = h.clone && h.clone.cloneNode(!0)
+			|| (typeof h.el =='string' ? document.createElement(h.el) : h.el);
+	var i
+		,toCsKey = function(s){
+			return s.replace(/([A-Z])/g,'-$1').toLowerCase();
+		};
+	if(o && (h.IF===undefined || h.IF) && (!h.q || h.q && (h.dQ = h.q instanceof Array ? $q(h.q[0], h.q[1]) : $q(h.q)) ) ){ //выполнять, если существует; h.dQ - результат селектора для функций IF,f
+		if(h.cl)
+			o.className = h.cl;
+		if(h.cs)
+			$x(o.style, h.cs);
+		if(at)
+			for(i in at){
+				if(i=='innerHTML') o[i] = at[i];
+				else o.setAttribute(i, at[i]);
+			}
+		if(h.on)
+			for(i in h.on)
+				o.addEventListener(i, h.on[i],!1);
+		h.apT && h.apT.appendChild(o);
+		h.prT && (h.prT.firstChild
+			? h.prT.insertBefore(o, h.prT.firstChild)
+			: h.prT.appendChild(o) );
+		h.bef && h.bef.parentNode.insertBefore(o, h.bef);
+		h.aft && (h.aft.nextSibling
+			? h.aft.parentNode.insertBefore(o, h.aft.nextSibling)
+			: h.aft.parentNode.appendChild(o) );
+		if(typeof h.f =='function')
+			h.f.apply(h, h.fA ||[]); //this - это h
+	}
+	return o;
+},
+$parents = function(clss, elem){ //регексп класса
+	for(var el = elem; el!=null && !RegExp(clss).test(el.className); el = el.parentNode);
+	return el;
+},
+$prev = function(clss, elem){
+	for(var el = elem; el!=null && !RegExp(clss).test(el.className); el = el.previousSibling);
+	return el;
+},
+$next = function(clss, elem){
+	for(var el = elem; el!=null && !RegExp(clss).test(el.className); el = el.nextSibling);
+	return el;
+},
+$getPosition = function(o){
+	var x =0, y =0;
+	while(o){
+		x += o.offsetLeft ||0;
+		y += o.offsetTop ||0;
+		o = o.offsetParent || o.parentNode;
+	}
+	return {x:x, y:y};
+},
+addRules = function(css){
+	var heads = document.getElementsByTagName('head');
+	if(heads.length){
+		var node = document.createElement('style');
+		node.type ='text/css';
+		node.appendChild(document.createTextNode(css));
+		heads[0].appendChild(node);
+	}
+},
+$ajax = function(h){
+	h.success = h.success || function(){};
+	h.error = h.error || function(){};
+	var xhr = new XMLHttpRequest();
+	xhr.open(h.method ||'get', h.url,!0);
+	xhr.send();
+	xhr.onreadystatechange = function(req){ //параметр - req (для req.responseText)
+		if(xhr.readyState ==4){
+			if(xhr.status ==200)
+				h.success(req, xhr);
+			else
+				h.error(req, xhr);
+		}
+	}
+};
+String.prototype.trim = function(s){var s = this ||s;
+	return s.replace(/(^\s+|\s+$)/g,'')};
---
+++ wk/hilitor.js
@@ -0,0 +1,56 @@
+﻿// Original JavaScript code by Chirp Internet: www.chirp.com.au
+// Please acknowledge use of this code by including this header.
+// modified by spmbt, 2013-05-18
+
+function Hilitor(el, tag){
+	var targetNode = el.attributes && el || document.getElementById(el) || document.body
+		,hiliteTag = tag ||'EM'
+		,skipTags = new RegExp('^(?:'+ hiliteTag +'|SCRIPT|FORM|SPAN)$')
+		,colors =['#ff6','#a0ffff','#9f9','#f9c3c3','#bbe']
+		,iColor =0
+		,wordColor =[]
+		,matchRegex ='';
+	this.setRegex = function(input){
+		input = input.replace(/^[^\wа-яА-ЯёЁ]+|[^\wа-яА-ЯёЁ]+$/g,'').replace(/[^\wа-яА-ЯёЁ'-]+/g,'|');
+		matchRegex = new RegExp('(^|[\\s\.,;:-])('+ input +')([\\s\.,;:-]|$)','i');
+	}; // note: \\b + cyrillic is not works
+	this.getRegex = function(){
+		return matchRegex.toString()//.replace(/^\/[\\s\.,;:-]\(|\)[\\s\.,;:-]\/i$/g,'').replace(/\|/g,' ');
+	};
+	this.hiliteWords = function(node){ // recursively apply word highlighting
+		if(node == null || !node || !matchRegex || skipTags.test(node.nodeName)) return;
+		var count =0;
+		if(node.hasChildNodes())
+			for(var i =0, nL = node.childNodes.length; i < nL; i++){
+				var h = this.hiliteWords(node.childNodes[i]);
+				count += h.count ||0;
+				if(h && h.nodeAdd){ nL += h.nodeAdd; i++; } //(добавилось 1-2 ноды)
+			}
+		if(node.nodeType ==3 && (nv = node.nodeValue) && (regs = matchRegex.exec(nv)) ){ // NODE_TEXT
+			if(!wordColor[regs[2].toLowerCase()])
+				wordColor[regs[2].toLowerCase()] = colors[iColor++ % colors.length];
+
+			var match = document.createElement(hiliteTag);
+			match.appendChild(document.createTextNode(regs[2]));
+			match.style.backgroundColor = wordColor[regs[2].toLowerCase()];
+			match.style.fontStyle ='inherit';
+			match.style.color ='#000';
+
+			var after = node.splitText(regs.index + regs[1].length);
+			after.nodeValue = after.nodeValue.substring(regs[2].length);
+			node.parentNode.insertBefore(match, after);
+			return {count: count +1, nodeAdd: 2};
+		}else return {count: count, nodeAdd: 0};
+	};
+	this.remove = function(){ // remove highlighting
+		var arr = document.getElementsByTagName(hiliteTag);
+		while(arr.length && (el = arr[0]))
+			el.parentNode.replaceChild(el.firstChild, el);
+	};
+	this.apply = function(input){ // start highlighting at target node
+		if(!input) return;
+		this.remove();
+		this.setRegex(input);
+		return this.hiliteWords(targetNode).count;
+	};
+}
